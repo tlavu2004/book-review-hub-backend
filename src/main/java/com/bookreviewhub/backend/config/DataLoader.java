35100a3731b44a13a4e2f45da4698e280d561af5
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2025-04-26 03:14:25 +0700

    refactor: remove redundant data seeding from DataLoader

--- src/main/java/com/bookreviewhub/backend/config/DataLoader.java
+++ src/main/java/com/bookreviewhub/backend/config/DataLoader.java
@@ -9,11 +9,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataLoader {
 
-    @Bean
-    CommandLineRunner loadData(BookRepository bookRepository) {
-        return args -> {
-            bookRepository.save(new Book(null, "Clean Code", "Robert C. Martin"));
-            bookRepository.save(new Book(null, "Effective Java", "Joshua Bloch"));
-        };
-    }
+//    @Bean
+//    CommandLineRunner loadData(BookRepository bookRepository) {
+//        return args -> {
+//            bookRepository.save(new Book(null, "Clean Code", "Robert C. Martin"));
+//            bookRepository.save(new Book(null, "Effective Java", "Joshua Bloch"));
+//        };
+//    }
 }
